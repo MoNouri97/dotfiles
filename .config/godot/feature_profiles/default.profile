ef130fe2f16c164472b9a13a6efd4a3e6e532d2e
Nouri Mohamed <mohamed.nouri.1997@gmail.com>
2025-07-18 19:52:05 +0200

    added godot

---
+++ .config/godot/feature_profiles/default.profile
@@ -0,0 +1,7 @@
+{
+	"disabled_classes": [],
+	"disabled_editors": [],
+	"disabled_features": [],
+	"disabled_properties": [],
+	"type": "feature_profile"
+}
---
+++ .config/godot/script_templates/Node/Simple.cs
@@ -0,0 +1,17 @@
+using GDHelpers;
+using Godot;
+
+public partial class _CLASS_ : _BASE_
+{
+    public override void _Notification(int what)
+    {
+        if (what == NotificationEnterTree)
+        {
+            this.WireOnReady();
+        }
+    }
+
+    public override void _Ready() { }
+
+    public override void _Process(double delta) { }
+}
