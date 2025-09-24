3199fb6cae7e1bcf4e82a1baacafacfdc9a320bd
Nouri Mohamed <mohamed.nouri.1997@gmail.com>
2025-09-24 10:43:15 +0200

    workspace lunch

--- my-scripts/godotWorkspace.sh
+++ my-scripts/godotWorkspace.sh
@@ -10,13 +10,16 @@ CURRENT_WORKSPACE=$(hyprctl activeworkspace -j | jq '.id')
 
 # Launch Godots in the current workspace
 hyprctl dispatch exec "[workspace $CURRENT_WORKSPACE] /home/mono/Downloads/Godots.x86_64"
+notify-send "Launched Godots in workspace $CURRENT_WORKSPACE"
 
 # Calculate the next workspace
 NEXT_WORKSPACE=$((CURRENT_WORKSPACE + 1))
+notify-send "Launched Kitty in workspace $NEXT_WORKSPACE"
 
 # Launch Kitty in the next workspace
 # hyprctl dispatch exec "[workspace $NEXT_WORKSPACE] kitty tmux"
-DIR=$(zoxide query fps)
+project="board"
+DIR=$(zoxide query $project)
 hyprctl dispatch exec "[workspace $NEXT_WORKSPACE] kitty tmux new-session -s mysession 'zsh -c \"cd $DIR && nvim . --listen ./godothost && zsh || zsh\"'"
 
 # Calculate the third workspace
@@ -25,6 +28,4 @@ THIRD_WORKSPACE=$((CURRENT_WORKSPACE + 2))
 # Launch Obsidian in the third workspace
 hyprctl dispatch exec "[workspace $THIRD_WORKSPACE] obsidian"
 
-notify-send "Launched Godots in workspace $CURRENT_WORKSPACE"
-notify-send "Launched Kitty in workspace $NEXT_WORKSPACE"
 notify-send "Launched Obsidian in workspace $THIRD_WORKSPACE"
