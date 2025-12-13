135f29a17b5c614aa2b3b7886294a3a9dad64025
Nouri Mohamed <mohamed.nouri.1997@gmail.com>
2025-12-13 21:52:43 +0100

    screenshot script

---
+++ my-scripts/screenshot_notify.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+DIR="$HOME/Pictures/Screenshots"
+mkdir -p "$DIR"
+
+FILE="$DIR/screenshot-$(date +%F_%H-%M-%S).png"
+
+hyde-shell grimblast save output "$FILE"
+notify-send $FILE
+
+notify-send \
+  -i "$FILE" \
+  "Screenshot saved" \
+  "$(basename "$FILE")"
+
+##!bash
+#grimblast save output
+#notify-send "screenshot"
+#
+#
