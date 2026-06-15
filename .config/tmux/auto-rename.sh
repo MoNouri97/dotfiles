b95306c86f388c35b8e59043d6c9939a52827d24
Nouri Mohamed <mohamed.nouri.1997@gmail.com>
2026-06-15 11:21:42 +0100

    mac: tmux autorename

---
+++ .config/tmux/auto-rename.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+path="$1"
+name=$(tmux display-message -p '#{window_name}')
+
+if [[ "$name" == "" || "$name" == "Window" ]]; then
+  case "$path" in
+  */mobile-repo*)
+    tmux setw automatic-rename off
+    tmux rename-window "MOBILE"
+    ;;
+  */codename-electron.git*)
+    tmux setw automatic-rename off
+    tmux rename-window "WEB"
+    ;;
+  esac
+fi
